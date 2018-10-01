1fb9423022cad4c178f253e87c5f1e18443c8127
Beatriz Mendes <bea.smendes98@gmail.com>
2018-10-01 16:30:08 +0100

    Changed 5 character block on read

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1;   
 
 
 
