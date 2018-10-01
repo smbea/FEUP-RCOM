f84da8d11d5ac127f60a9597d93aea213991a53b
Beatriz Mendes <bea.smendes98@gmail.com>
2018-10-01 16:31:16 +0100

    Readinh and writting back

--- noncanonical.c
+++ noncanonical.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
@@ -15,6 +16,7 @@ volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
+
     int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
@@ -33,7 +35,7 @@ int main(int argc, char** argv)
   */
   
     
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    fd = open(argv[1], O_RDWR | O_NOCTTY);
     if (fd <0) {perror(argv[1]); exit(-1); }
 
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
@@ -50,13 +52,13 @@ int main(int argc, char** argv)
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1; 
 
 
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
+    leitura do(s) prï¿½ximo(s) caracter(es)
   */
 
 
@@ -70,23 +72,28 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-
+ 	res = 0;
     while (STOP==FALSE) {       /* loop for input */
-      res = read(fd,buf,255);   /* returns after 5 chars have been input */
-      buf[res]=0;               /* so we can printf... */
-      printf(":%s:%d\n", buf, res);
-      if (buf[0]=='z') STOP=TRUE;
+		printf("Res start\n");
+		ssize_t count = read(fd,buf+res,1);
+		if(count > 0) {
+			if(buf[res++] == '\0')
+				STOP=TRUE;
+		} 
     }
+	
+	printf(":%s:%d\n", buf, res);
+
+	res = write(fd,buf,res);
+    printf("%d bytes written\n", res);
 
 
 
   /* 
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guiï¿½o 
   */
-
-
-
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
     return 0;
 }
+
