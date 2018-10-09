fc8f46956afe6e164b232f35e93fd5d858ba66b7
4-Z3r0 <32849465+4-Z3r0@users.noreply.github.com>
2018-10-09 15:02:06 +0100

    Added debug info

--- stateMachine.c
+++ stateMachine.c
@@ -9,67 +9,6 @@ struct tram{
 	char header;
 };
 
-
-int main()
-{
-	// the current state of the machine
-	State currentState;
-	// the function that processes the current state
-	int (*currentStateFunc)(State, byte) = &stateStart;
-	int flag1;
-	char c;
-	struct tram alpha;
-	int tempByte = 0;
-	
-	while(1)
-	{
-		do //check flag
-		{ 
-			tempByte = readByte();
-		}while(tempByte != alpha.firstFlag)
-	
-		do //check message type
-		{ 
-			tempByte = readByte();
-		}while(tempByte != 3 && tempByte != 1 && tempByte != alpha.firstFlag)
-	
-		if(tempByte == 3)
-			alpha.messageType = 0;
-		else if(tempByte == 1)
-			alpha.messageType = 1;
-		else
-			continue;
-	
-		do
-		{
-			tempByte = readByte();
-		}while(tempByte != 0x02 && tempByte != 0x0B && tempByte != 0x07 && /*!*/ tempByte != 0x05 && tempByte != 0x01 && tempByte != alpha.firstFlag)
-		
-		switch(tempByte)
-		{
-			case 0x02:
-				alpha.header = 'S';
-				break;
-			case 0x0B:
-				alpha.header = 'D';
-				break;
-			case 0x07:
-				alpha.header = 'U';
-				break;
-			case 0x05:
-				alpha.header = 'R';
-				break;
-			case 0x01:
-				alpha.header = 'J';
-				break;
-			default:
-				continue;
-		}
-		break;
-	}	
-}
-
-
 /**
  * Processes the input when the machine is in the Start state
  */
@@ -79,12 +18,14 @@ int stateStart(stateMachine *st, byte input) {
 	
 	// if the input matches 
 	if(input == FLAG) {
+		printf("Transitioned to flag_rcv state\n");
 		// transitate the state to FLAG_RCV and update function
-		st->currentState == FLAG_RCV;
+		st->currentState = FLAG_RCV;
 		st->currentStateFunc = &stateFlag;
 		return 0;
 	} else {
 		// stays on Start state
+		printf("Staying in start state\n");
 		return 0;
 	}
 }
@@ -97,11 +38,13 @@ int stateFlag(stateMachine *st, byte input) {
 		// stays on the Flag state
 		return 0;
 	} else if(input == SENT_BY_EMISSOR || input == SENT_BY_RECEPTOR) {
+		printf("Transitioned to a_rcv state\n");
 		st->currentState = A_RCV;
 		st->currentStateFunc = &stateAddress;
 		return 0;
 	} else {
 		// unknown input, transitate to Start state
+		printf("Unknown input, back to start\n");
 		st->currentState = START;
 		st->currentStateFunc = &stateStart;
 		return 0;
@@ -114,16 +57,19 @@ int stateAddress(stateMachine *st, byte input) {
 	
 	if(input == SET || input == DISC || input == UA) {
 		// do something I guess...
-		st->currentState = BCC_RCV;
+		printf("Transitioned to c_rcv state\n");
+		st->currentState = C_RCV;
 		st->currentStateFunc = &stateProtection;
 		return 0;
 	} else if(input == FLAG) {
 		// go back to flag state
+		printf("Got FLAG, back to flag state state\n");
 		st->currentState = FLAG;
 		st->currentStateFunc = &stateFlag;
 		return 0;
 	} else {
 		// unknown input
+		printf("Unknown input, back to start\n");
 		st->currentState = START;
 		st->currentStateFunc = &stateStart;
 	}
@@ -134,9 +80,11 @@ int stateProtection(stateMachine *st, byte input) {
 		return -1;
 	
 	if(input == FLAG) {
+		printf("Got flag, back to flag state\n");
 		st->currentState = FLAG;
 		st->currentStateFunc = stateFlag;
-	} else if (input == SENT_BY_EMISSOR^SET) {
+	} else if (input == (SENT_BY_EMISSOR^SET)) {
+		printf("Transitioned to bcc_rcv state\n");
 		st->currentState = BCC_RCV;
 		st->currentStateFunc = stateBCC;
 	} else {
@@ -151,5 +99,7 @@ int stateBCC(stateMachine *st, byte input) {
 
 	if(input == FLAG) {
 		printf("end\n");
+	} else {
+		printf("TODO\n");
 	}
 }
