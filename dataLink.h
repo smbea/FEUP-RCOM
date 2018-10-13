ab437e7e024e54c7250eefac6bf1f6692c4f95b6
Unknown <bea.smendes98@gmail.com>
2018-10-13 12:11:38 +0100

    Revert "llopen for receiver and emissor"
    This reverts commit 02feefc791bd22ad2b82bf6b95f422bb1fee593b.

--- dataLink.h
+++ dataLink.h
@@ -1,7 +1,6 @@
 #ifndef DATALINK_H
 #define DATALINK_H
 
-#include "stateMachine.h"
 
 //variables
 #define BAUDRATE B38400
@@ -11,11 +10,8 @@ enum port{
   COM2 = 2
 };
 
-stateMachine st;
-
 //functions
 int llopen(int port, int f);
-int llclose(int fd);
 
 
 #endif
