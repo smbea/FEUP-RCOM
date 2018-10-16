aa7a7ed8c86c7e5ffbc361a856a1510e5e8da09b
Afonso <afonsojoseazevedo@gmail.com>
2018-10-16 17:15:59 +0100

    Finished implementing llclose

--- dataLink.c
+++ dataLink.c
@@ -11,103 +11,125 @@
 #include <string.h>
 #include <signal.h>
 
-struct termios oldtio,newtio;
-int send_flag=1, conta=1;
+struct termios oldtio, newtio;
+int send_flag = 1, conta = 1;
 
-int main(int argc, char** argv){
-  int r_e_flag = 0;
-  int port, fd;
+int main(int argc, char **argv)
+{
+	int r_e_flag = 0;
+	int port, fd;
 
-  if(argc<3){
-    printf("Incorrect number of arguments\n");
-    exit(1);
-  }
+	if (argc < 3)
+	{
+		printf("Incorrect number of arguments\n");
+		exit(1);
+	}
 
-  //port
-  if(atoi(argv[1])==0) port =  COM1;
-  else if(atoi(argv[1])==1) port = COM2;
-  else{
-    printf("First argument invalid\n");
-    exit(1);
-  }
+	//port
+	if (atoi(argv[1]) == 0)
+		port = COM1;
+	else if (atoi(argv[1]) == 1)
+		port = COM2;
+	else
+	{
+		printf("First argument invalid\n");
+		exit(1);
+	}
 
-//
-  if(atoi(argv[2])==0) r_e_flag = EMISSOR_FLAG;
-  else if(atoi(argv[2])==1) r_e_flag = RECEIVER_FLAG;
-  else{
-    printf("Second argument invalid\n");
-    exit(1);
-  }
+	//
+	if (atoi(argv[2]) == 0)
+		r_e_flag = EMISSOR_FLAG;
+	else if (atoi(argv[2]) == 1)
+		r_e_flag = RECEIVER_FLAG;
+	else
+	{
+		printf("Second argument invalid\n");
+		exit(1);
+	}
 
-  fd = llopen(port,r_e_flag);
+	fd = llopen(port, r_e_flag);
 
-  fflush(NULL);
+	fflush(NULL);
 
+	if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+	{
+		perror("tcsetattr");
+		exit(-1);
+	}
 
-  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-    perror("tcsetattr");
-    exit(-1);
-  }
+	close(fd);
 
-  close(fd);
-
-  return 0;
+	return 0;
 }
 
+int llopen(int port, int r_e_flag)
+{
+	int fd;
+	char *portName;
 
-int llopen(int port, int r_e_flag){
-  int fd;
-  char * portName;
+	if (port == COM1)
+		portName = "/dev/ttyS0";
+	else if (port == COM2)
+		portName = "/dev/ttyS1";
+	else
+		return -1;
 
-  if(port==COM1) portName = "/dev/ttyS0";
-  else if (port==COM2) portName = "/dev/ttyS1";
-  else return -1;
+	fd = open(portName, O_RDWR | O_NOCTTY);
+	if (fd < 0)
+	{
+		perror(portName);
+		exit(-1);
+	}
 
-  fd = open(portName, O_RDWR | O_NOCTTY );
-  if (fd <0) {perror(portName); exit(-1); }
+	if (tcgetattr(fd, &oldtio) == -1)
+	{ /* save current port settings */
+		perror("tcgetattr");
+		exit(-1);
+	}
 
-  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-    perror("tcgetattr");
-    exit(-1);
-  }
+	bzero(&newtio, sizeof(newtio));
+	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	newtio.c_iflag = IGNPAR;
+	newtio.c_oflag = 0;
 
-  bzero(&newtio, sizeof(newtio));
-  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-  newtio.c_iflag = IGNPAR;
-  newtio.c_oflag = 0;
+	/* set input mode (non-canonical, no echo,...) */
+	newtio.c_lflag = 0;
 
-  /* set input mode (non-canonical, no echo,...) */
-  newtio.c_lflag = 0;
+	newtio.c_cc[VTIME] = 1; /* inter-character timer unused */
+	newtio.c_cc[VMIN] = 0;
 
-  newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-  newtio.c_cc[VMIN]     = 0;
+	tcflush(fd, TCIOFLUSH);
 
-  tcflush(fd, TCIOFLUSH);
+	if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+	{
+		perror("tcsetattr");
+		exit(-1);
+	}
 
-  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-    perror("tcsetattr");
-    exit(-1);
-  }
+	printf("New termios structure set\n");
 
-  printf("New termios structure set\n");
+	if (r_e_flag == RECEIVER_FLAG)
+		open_receiver(fd);
+	else if (r_e_flag == EMISSOR_FLAG)
+		open_emissor(fd);
 
-  if(r_e_flag == RECEIVER_FLAG) open_receiver(fd);
-  else if(r_e_flag == EMISSOR_FLAG) open_emissor(fd);
-
-  return fd;
+	return fd;
 }
 
-void open_receiver(int fd){
+void open_receiver(int fd)
+{
 
-  	st.currentState = START;
+	st.currentState = START;
 	st.currentStateFunc = &stateStart;
 
 	unsigned char frame;
-	while (st.currentState != END) {
-		if (read(fd, &frame, 1) > 0){
-			(*st.currentStateFunc)(&st, frame);
+	while (st.currentState != END)
+	{
+		if (read(fd, &frame, 1) > 0)
+		{
+			(*st.currentStateFunc)(&st, frame, SET);
 
-    		printf("received: %X\n", frame);
+			printf("received: %X\n", frame);
 		}
 	}
 
@@ -116,160 +138,235 @@ void open_receiver(int fd){
 	sleep(3);
 }
 
-void atende(int signo){
+void atende(int signo)
+{
 	printf("alarme # %d\n", conta);
-	send_flag=1;
+	send_flag = 1;
 	conta++;
 }
 
-void open_emissor(int fd){
- 
-  int res;
+void open_emissor(int fd)
+{
 
-  st.currentState = START;
-  st.currentStateFunc = &stateStart;
+	int res;
 
-  struct sigaction act;
-  act.sa_handler = atende;
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = 0;
+	st.currentState = START;
+	st.currentStateFunc = &stateStart;
 
-  if (sigaction(SIGALRM, &act,NULL) == -1){
-    printf("Error\n");
-    exit(-1);
-  }
+	struct sigaction act;
+	act.sa_handler = atende;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
 
-  unsigned char teste;
+	if (sigaction(SIGALRM, &act, NULL) == -1)
+	{
+		printf("Error\n");
+		exit(-1);
+	}
 
-  while(conta < 4){
-    if(send_flag){
+	unsigned char teste;
 
-        printf("writing message\n");
-        send_SET(fd);
+	while (conta < 4)
+	{
+		if (send_flag)
+		{
 
-        alarm(3);                 // activa alarme de 3s
-        printf("sent alarm\n");
-        send_flag=0;
-    }
+			printf("writing message\n");
+			send_SET(fd);
 
-    while(1){
-        res = read(fd,&teste,1);
-      	if (res > 0){
-        	printf("%x\n", teste);
-       		(*st.currentStateFunc)(&st, teste);
-      		}
-		if(st.currentState == END || send_flag) break;
-        }
+			alarm(3); // activa alarme de 3s
+			printf("sent alarm\n");
+			send_flag = 0;
+		}
 
-     if(st.currentState == END) return;
-  }
+		while (1)
+		{
+			res = read(fd, &teste, 1);
+			if (res > 0)
+			{
+				printf("%x\n", teste);
+				(*st.currentStateFunc)(&st, teste, UA);
+			}
+			if (st.currentState == END || send_flag)
+				break;
+		}
+
+		if (st.currentState == END)
+			return;
+	}
 }
 
-
-void send_SET(int fd) {
+void send_SET(int fd)
+{
 	unsigned char buf[5] = {FLAG, SENT_BY_EMISSOR, SET, SENT_BY_EMISSOR ^ SET, FLAG};
 	write(fd, buf, 5);
 	printf("sent SET packet\n");
 }
 
-void send_UA(int fd) {
+void send_UA(int fd)
+{
 	// TODO
 	unsigned char buf[5] = {FLAG, SENT_BY_RECEPTOR, UA, SENT_BY_RECEPTOR ^ UA, FLAG};
 	write(fd, buf, 5);
 	printf("sent UA packet\n");
 }
 
-void send_DISC(int fd, int r_e_flag){
-	unsigned char buf[5];	
-	
-	if(!r_e_flag)	
+int send_DISC(int fd, int r_e_flag)
+{
+	unsigned char buf[5];
+
+	if (r_e_flag == SENT_BY_EMISSOR)
 		buf[5] = {FLAG, SENT_BY_EMISSOR, DISC, SENT_BY_EMISSOR ^ DISC, FLAG};
-	else
+	else if (r_e_flag == SENT_BY_RECEPTOR)
 		buf[5] = {FLAG, SENT_BY_RECEPTOR, DISC, SENT_BY_RECEPTOR ^ DISC, FLAG};
+	else
+		return -1;
 
 	write(fd, buf, 5);
-	
+
 	printf("sent DISC packet\n");
+
+	return 0;
 }
 
-void close_receiver(int fd){
-	
+//How to handle an error in a last UA sent. After sending the last UA the receiver closes/disconnects, if this UA is not received by the receiver
+//should the receiver time-out and also disconnect/close or should it stay in a infinite wait for the last UA?
+void close_receiver(int fd)
+{
+
 	//Waits for first DISC flag
 	st.currentState = START;
 	st.currentStateFunc = &stateStart;
 
 	unsigned char frame;
-	//currentStateFunc functions should be changed to force a specific flag(UA/SET/DISC)
-	while (st.currentState != END) {
-		if (read(fd, &frame, 1) > 0){
-			(*st.currentStateFunc)(&st, frame);
+	while (st.currentState != END)
+	{
+		if (read(fd, &frame, 1) > 0)
+		{
+			(*st.currentStateFunc)(&st, frame, DISC);
 
-    		printf("received: %X\n", frame);
+			printf("received: %X\n", frame);
 		}
 
-	}//DISC flag received
-
-	
+	} //DISC flag received
 
 	//Waits for UA flag to end the data link, while it does not receive UA flag tries to resend DISC flag
 	st.currentState = START;
-  	st.currentStateFunc = &stateStart;
+	st.currentStateFunc = &stateStart;
 
-  	struct sigaction act;
-  	act.sa_handler = atende;
-  	sigemptyset(&act.sa_mask);
-  	act.sa_flags = 0;
+	struct sigaction act;
+	act.sa_handler = atende;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
 
-	 if (sigaction(SIGALRM, &act,NULL) == -1){
-	    printf("Error\n");
-	    exit(-1);
-	 }
+	if (sigaction(SIGALRM, &act, NULL) == -1)
+	{
+		printf("Error\n");
+		exit(-1);
+	}
 
- 	unsigned char teste;
+	unsigned char teste;
 
-  	while(conta < 4){
-		if(send_flag){
+	while (conta < 4)
+	{ //4 tentativas de alarme
+		if (send_flag)
+		{
 
-		    printf("writing message\n");
+			printf("writing message\n");
 			send_DISC(fd, RECEIVER_FLAG); //sends DISC flag back to the emissor
 
-		    alarm(3);                 // activa alarme de 3s
-		    printf("sent alarm\n");
-		    send_flag=0;
+			alarm(3); // activa alarme de 3s
+			printf("sent alarm\n");
+			send_flag = 0;
 		}
 
-		while(1){
-		    res = read(fd,&teste,1);
-		  	if (res > 0){
-		    	printf("%x\n", teste);
-		   		(*st.currentStateFunc)(&st, teste);
-		  		}
-			if(st.currentState == END || send_flag) break;
-		    }
+		while (1)
+		{
+			res = read(fd, &teste, 1);
+			if (res > 0)
+			{
+				printf("%x\n", teste);
+				(*st.currentStateFunc)(&st, teste, UA);
+			}
+			if (st.currentState == END || send_flag)
+				break;
+		}
 
-		 if(st.currentState == END) return;
-	  }
+		if (st.currentState == END)
+			return;
+	}
 }
 
-int close_emissor(int fd){
-	//TODO
+void close_emissor(int fd)
+{
+	int res;
+
+	st.currentState = START;
+	st.currentStateFunc = &stateStart;
+
+	struct sigaction act;
+	act.sa_handler = atende;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+
+	if (sigaction(SIGALRM, &act, NULL) == -1)
+	{
+		printf("Error\n");
+		exit(-1);
+	}
+
+	unsigned char teste;
+
+	while (conta < 4)
+	{
+		if (send_flag)
+		{
+
+			printf("writing message\n");
+			send_DISC(fd, SENT_BY_EMISSOR);
+
+			alarm(3); // activa alarme de 3s
+			printf("sent alarm\n");
+			send_flag = 0;
+		}
+
+		while (1)
+		{
+			res = read(fd, &teste, 1);
+			if (res > 0)
+			{
+				printf("%x\n", teste);
+				(*st.currentStateFunc)(&st, teste, DISC);
+			}
+			if (st.currentState == END || send_flag)
+				break;
+		}
+
+		if (st.currentState == END)
+		{
+			send_UA(fd);
+			return;
+		}
+	}
 }
 
-int llclose(int fd){
+int llclose(int fd)
+{
 
- 
-	if(r_e_flag == RECEIVER_FLAG) close_receiver(fd);
-  else if(r_e_flag == EMISSOR_FLAG) close_emissor(fd);
-	
+	if (r_e_flag == RECEIVER_FLAG)
+		close_receiver(fd);
+	else if (r_e_flag == EMISSOR_FLAG)
+		close_emissor(fd);
 
 	fflush(NULL);
 
-  	if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-    	perror("tcsetattr");
-    	exit(-1);
-  	}
-	
+	if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+	{
+		perror("tcsetattr");
+		exit(-1);
+	}
+
 	close(fd);
-	
+
 	return 1; //positive return value for success
 }
