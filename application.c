79d1e641ea2a85aa514bfd71beee12919665646d
Beatriz Mendes <bea.smendes98@gmail.com>
2018-10-26 18:18:11 +0100

    Packet generation implemented
    !Not tested data generation! (Afonso)

--- application.c
+++ application.c
@@ -8,45 +8,60 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
-#include <math.h>
 
-int log2(int x)
-{
-	return (int)(log10(x)/log10(2.0) + 0.5);
+
+int generateDataPacket(char sequenceNumber, char* data, char* packet){
+
+	packet[0] = 0x01;
+	packet[1] = sequenceNumber % 255;
+	int dataSize = sizeof(data);
+	int l2 = dataSize / 256;
+	int l1 = dataSize % 256;
+	packet[2] = (char)l2;
+	packet[3] = (char)l1;
+	int h;
+	int index = 3;
+	for(h = 0; h < dataSize; h++)
+	{
+		packet[++index] = data[h];
+	}
+	return index;
 }
 
 
-char* generateControlPacket(int start_end_flag, char* file_name, char* file_size)
+
+int generateControlPacket(int start_end_flag, char* file_name, char* file_size, char* packet)
 {
 	int i = 0, j = 0;
 
-	char* packet;
 
-	if(start_end_flag == START)
+	if(start_end_flag == APP_START)
 		packet[0] = 0x02;
-	else if(start_end_flag == END)
+	else if(start_end_flag == APP_END)
 		packet[0] = 0x03;
 
 	i++;
 
 	packet[i++] = FILE_SIZE;
-	packet[i++] = (char)strlen(file_size);
+	packet[i++] = (char)strlen(file_size); //NOPE
 
 	for(j = 0; j < strlen(file_size); j++)
 		packet[i++] = file_size[j];
 
 	packet[i++] = FILE_NAME;
-	packet[i++] = (char)strlen(file_name);
+	packet[i++] = (char)strlen(file_name); //NOPE
 
 	for(j = 0; j < strlen(file_name); j++)
 		packet[i++] = file_name[j];
 
-	return packet;
+	return i;
 }
 
 int main(int argc, char** argv){
 
 	int port;
+	int file_size;
+	applicationLayer application;
 
 	if (argc < 4)
 	{
@@ -82,19 +97,38 @@ int main(int argc, char** argv){
 	if(status == TRANSMITTER) application.send_fd = open(send_file,O_RDWR);
 	else application.send_fd = open(send_file,O_WRONLY);
 
+
 	if (application.send_fd < 0)
 	{
 		perror(send_file);
 		exit(-1);
 	}
 
+	file_size = getFileSize(application.send_fd);
+
+	char fs[4];
+	sprintf(fs, "%d", file_size);
+
+	/*int g;
+	for(g= 0; g < 12; g++)
+	printf("%x\n", fs[g]);*/
+	//debugging
 
 	application.send_fd = llopen(port, status);
+
+	char packet[255];
+	int packet_size = generateControlPacket(0x02, "teste1", fs, packet);
+
+	int k;
+	printf("packet:");
+	for(k= 0; k < packet_size; k++)
+		printf("%x\n", packet[k]);
+
+	llwrite(application.send_fd, packet, packet_size);
 
 	return 0;
 
 }
-
 
 int getFileSize(int fd) {
 	struct stat statbuf;
