10d84116a875a5304c12d8b95e0529a0fd4bb3a2
Unknown <bea.smendes98@gmail.com>
2018-11-01 13:38:35 +0000

    testing

--- dataLink.c
+++ dataLink.c
@@ -568,7 +568,7 @@ int llread(int fd, unsigned char *buffer)
 		res = read(fd, &buf, 1);
 		if (res > 0)
 		{
-			//printf(" %x ", buf);
+			printf(" %x ", buf);
 			(*st.currentStateFunc)(&st, buf);
 			if(k == 2) ns = buf;
 			k++;
